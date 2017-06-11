5cc1ffb79068a3e7114464b238b62802f4362c6b
xingxiant <1398374588@qq.com>
2017-06-11 23:53:13 +0800

    comment

--- src/main/java/org/imooc/controller/api/ApiController.java
+++ src/main/java/org/imooc/controller/api/ApiController.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.imooc.bean.Ad;
 import org.imooc.bean.BusinessList;
+import org.imooc.dto.AdDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +22,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class ApiController {
 	//@ResponseBody  //表示该方法的返回结果直接写入HTTP response body中,用于返回json数据
 	@RequestMapping(value="/homead",method=RequestMethod.GET)
-	public List<Ad> homead() throws JsonParseException, JsonMappingException, IOException {
+	public List<AdDto> homead() throws JsonParseException, JsonMappingException, IOException {
 		ObjectMapper mapper=new ObjectMapper();
 		String src="[{\"title\":\"暑假5折\",\"img\":\"http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191639092-2000037796.png\",\"link\":\"http://www.imooc.com/wap/index\"},{\"title\":\"特价出国\",\"img\":\"http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191648124-298129318.png\",\"link\":\"http://www.imooc.com/wap/index\"},{\"title\":\"亮亮车\",\"img\":\"http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191653983-1962772127.png\",\"link\":\"http://www.imooc.com/wap/index\"},{\"title\":\"学钢琴\",\"img\":\"http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191700420-1584459466.png\",\"link\":\"http://www.imooc.com/wap/index\"},{\"title\":\"电影\",\"img\":\"http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191706733-367929553.png\",\"link\":\"http://www.imooc.com/wap/index\"},{\"title\":\"旅游热线\",\"img\":\"http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191713186-495002222.png\",\"link\":\"http://www.imooc.com/wap/index\"}]";
-		return mapper.readValue(src, new TypeReference<List<Ad>>() {
+		return mapper.readValue(src, new TypeReference<List<AdDto>>() {
 		});
 		 
 		
